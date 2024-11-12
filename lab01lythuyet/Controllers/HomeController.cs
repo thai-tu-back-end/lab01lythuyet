7837fed3ed3d61f44c02eeb8542fcec294a4b068
Admin <Admin@HYN>
2024-11-12 08:20:00 +0700

    Add project files.

---
+++ lab01lythuyet/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab01lythuyet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Photoshoot_Effect()
+        {
+            return View();
+        }
+        public ActionResult ancientarchitecture()
+        {
+            return View();
+        }
+
+    }
+}
